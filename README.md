52222e2102c201c247e4f91e5c7132a7a61f6f58
Francisco Junior <franciscosantos69420@gmail.com>
2025-07-30 06:46:09 -0300

    Finalidades prontas, mas com bugs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # amigoSecreto
+
+##TODO
+-consertar bugs da geração de IDS da função adicionarAmigo()
+-remoção do nomes da listaNomes apos removerAmigo()
+-mudança cor de background apos sorteio no norme sorteiado da primeira lista
--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let listaNomes = [];
+let qntPessoas = 0;
+let indexList = 0;
 let objEntrada = document.getElementById("amigo");
+let listFriends = document.getElementById("listaAmigos");
+let resultado = document.getElementById('resultado');
 
 function validarEntrada(entrada) {
     if(entrada == ""){
@@ -8,15 +12,42 @@ function validarEntrada(entrada) {
     return true;
 }
 
+function removerAmigo(id) {
+    let target = document.getElementById(id);
+    listaNomes();
+    listFriends.removeChild(target);
+    qntPessoas--;
+}
+
 function adicionarAmigo() {
-    if (validarEntrada(objEntrada.value)){
+    let entradaInput = objEntrada.value;
+    if (validarEntrada(entradaInput)){
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
+        let idName = `friend${indexList}`;
+        
+        button.setAttribute('onclick',`removerAmigo('${idName}')`);
+        listItem.setAttribute('id',`${idName}`);
+    
+        button.innerHTML = 'Remover';
+        listItem.innerHTML = `${entradaInput}`;
+        listItem.appendChild(button);
+        listFriends.appendChild(listItem);
+
+        listaNomes.push(entradaInput);
+        indexList++;
+        qntPessoas++;
     }
     else {
         alert('insira um nome');
     }
-    console.log('teste empty string');
 }
 
 function sortearAmigo() {
-    
+    let numSorteio = parseInt(Math.random() * qntPessoas);
+    let txtResultado = document.createElement('li');
+    //let amigoSorteado = document.getElementById(`friend${numSorteio}`);
+
+    txtResultado.innerText = listaNomes[numSorteio];
+    resultado.appendChild(txtResultado);
 }
