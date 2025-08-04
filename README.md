9d0d5266985c7a9b816b47faf7938b796d588f7b
Francisco Junior <franciscosantos69420@gmail.com>
2025-08-04 16:10:26 -0300

    Comentar codigo/atualiza README

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# amigoSecreto
+<h1 align="center"> Amigo Secreto </h1>
 
-##TODO
--comentar o codigo
+![badge situação projeto](https://img.shields.io/badge/Em_desenvonvimento-%2332a895)
+![bagge versao](https://img.shields.io/badge/Prototipo-%23a832a8)
+
+[Link do projeto](https://github.com/JooJnin/amigoSecreto/deployments/github-pages)
--- app.js
+++ app.js
@@ -1,17 +1,21 @@
+//lista nomes tem a relação ID : nome
+//listaIDS se usa o Math.random para sortear o numero do index e acessar listaNomes 
 let listaNomes = {'sorteado':'sorteado'};
 let listaIDS = [];
 let notFirstTime = false;
 let qntPessoas = 0;
 
+//
 let objEntrada = document.getElementById("amigo");
 let listFriends = document.getElementById("listaAmigos");
 let resultado = document.getElementById('resultado');
 
+//gera ID para pintar nomes sorteados e utilizar o botão remover
 function gerarID(nome){
 
     let newID = nome;
     
-    //let i = qntPessoas;
+    //adiciona um 'a' no começo se o ID já existir no objeto listaNomes
     if(newID in listaNomes){
         newID = 'a' + newID;
         
@@ -32,6 +36,7 @@ function validarEntrada(entrada) {
     return true;
 }
 
+//gerada dinamicamente na criação dos elementos em adicionarAmigo()
 function removerAmigo(id) {
     let target = document.getElementById(id);
 
@@ -52,11 +57,16 @@ function adicionarAmigo() {
         let listItem = document.createElement('li');
         let button = document.createElement('button');
         
+        //define ID para ambos elementos serem removidos
+        //
+        //acredito que possa ser feito com div para executar apenas uma vez
+        //tanto aqui quanto em removerAmigo()
         button.setAttribute('onclick',`removerAmigo('${idName}')`);
         listItem.setAttribute('id',`${idName}`);
-    
-        button.innerHTML = 'Remover';
-        listItem.innerHTML = `${entradaInput}`;
+        
+        button.innerText = 'Remover';
+        listItem.innerText = `${entradaInput}`;
+
         listItem.appendChild(button);
         listFriends.appendChild(listItem);
 
@@ -68,6 +78,9 @@ function adicionarAmigo() {
 }
 
 function sortearAmigo() {
+    //caso seja primeira vez sorteado, esse if não executará
+    //porque não há childNode dentro do UL de classe 'resultado'
+    //uma alternativa é verificar se o innerText dessa UL seja vazio
     if (notFirstTime) {
         let toChangeLI = document.getElementById('sorteado');
         
@@ -75,6 +88,8 @@ function sortearAmigo() {
     }
     notFirstTime = true;
     
+    //sorteia cor, e numero dentre a quantidade de nomes na lista
+    //recebe nome pelo index da listaIDS > listaNomes
     let bColor = parseInt(Math.random() * 16777215);
     let numSorteio = parseInt(Math.random() * qntPessoas);
     let txtResultado = document.createElement('li');
@@ -82,6 +97,8 @@ function sortearAmigo() {
     
     txtResultado.setAttribute('id','sorteado');
     
+    //se a lista para o sorteio estiver vazia isso não executara
+    //acredito que seja um vestigio de codigo macarrao e que existe forma melhor de fazer isso
     if(amigoSorteado != null){
         amigoSorteado.style.backgroundColor = '#' + bColor.toString(16);
         
